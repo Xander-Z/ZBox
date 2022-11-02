b25793667ad522ef4fd63998b90d213e06e96777
邹雄心 <928457592@qq.com>
2022-11-02 17:28:13 +0800

    Add KAIST evaluation tool

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'ZBox',
+    version = '1.0',
+    author = 'Xander-Z',
+    author_email= '928457592@qq.com',
+    url = 'https://github.com/Xander-Z/ZBox',
+    description= 'a tool box of Xander-Z',
+    packages = ['kaist_eval']
+)
