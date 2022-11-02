c57dadaabe9f757234347a874ee8805ed3bd717a
邹雄心 <928457592@qq.com>
2022-11-02 19:11:34 +0800

    Add KAIST evaluation tool

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     author_email= '928457592@qq.com',
     url = 'https://github.com/Xander-Z/ZBox',
     description= 'a tool box of Xander-Z',
-    packages = ['ZBox.miss_rate_eval']
+    packages = ['ZBox', 'ZBox.miss_rate_eval']
 )
